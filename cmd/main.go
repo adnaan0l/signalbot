f74e500547ab015c8e4cdbf9b43b1872cd165b27
Shaldrake, Adnan <ashaldrake@gmail.com>
2023-03-12 13:26:20 +0530

    add: Added buy/sell analysis

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,27 @@
 package main
 
-import "adnan/binance-bot/pkg/app"
+import (
+	"adnan/binance-bot/pkg/app"
+	"fmt"
+
+	"github.com/sdcoffey/techan"
+)
 
 func main() {
-	app.GetTickerData()
+	limit := 24
+	app.GetTickerData(limit)
+	series := app.GetTimeSeries("ETHUSDT")
+	strategy := app.Strategy1(series)
+
+	record := techan.NewTradingRecord()
+	for i := 20; i < limit; i++ {
+		if strategy.ShouldEnter(i, record) {
+			fmt.Printf("Buy at %s", series.Candles[i].MinPrice)
+			break
+		} else if strategy.ShouldExit(i, record) {
+			fmt.Printf("Sell at %s", series.Candles[i].MaxPrice)
+			break
+		}
+	}
+	fmt.Println("Do Nothing")
 }
